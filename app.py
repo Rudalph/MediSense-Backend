211fad8d503fc956810041d3964f73cf31c1a3c8
Rudalph <gonsalvesrudalph@gmail.com>
2024-09-17 15:57:16 +0530

    genai api integrated

--- app.py
+++ app.py
@@ -103,6 +103,34 @@ def get_brand_details():
         return jsonify({'error': 'Brand not found'}), 404
     
     
+
+import re
+import google.generativeai as genai
+
+model = genai.GenerativeModel('gemini-pro')
+@app.route('/genai', methods=['POST'])
+def generate_recommendations():
+    try:
+        # Get the question from the request data
+        question = request.json.get('question')
+        
+        # Generate response based on the question
+        response = model.generate_content(question)
+        
+        # Format the response
+        # recommendations = textwrap.indent(response.text, '> ')
+        
+        
+        recommendations = re.sub(r'[>*]+', '', response.text).strip()
+        print(recommendations)
+        
+        return jsonify({'recommendations': recommendations}), 200
+    except Exception as e:
+        return jsonify({'error': str(e)}), 400
+
+
+
+
 import requests
 from apscheduler.schedulers.background import BackgroundScheduler
    
@@ -127,5 +155,5 @@ scheduler.start()
 
 
 
-if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+# if __name__ == '__main__':
+#     app.run(debug=True, port=5000)
